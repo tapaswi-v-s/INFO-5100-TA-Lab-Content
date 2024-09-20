273896e0f93b34e088cf5f911792e6a169debaf2
Tapaswi <satyapanthi.t@northeastern.edu>
2024-09-20 17:17:35 -0400

    Lab 2 Session

---
+++ LAB-2/LabSession2/src/user_profile/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package user_profile;
+
+/**
+ *
+ * @author tapas
+ */
+public class User {
+    private String name;
+    private String gender;
+    private double age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+    
+    
+}   
